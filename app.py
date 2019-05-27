ccdf87dd3f39655dd8f9e9193e51f924bb5c6681
Faith <jamienibtong@gmail.com>
2019-05-28 07:22:50 +0800

    add

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
+def home():
+    # shows the home page
     return 'Home Page'
+
 
 @app.route('/about')
 def about():
+    # shows the about page
     return 'About'
+
 
 @app.route('/user/<username>')
 def user_profile(username):
-    return 'User %s' username
+    # shows user profile
+    return 'User %s' % username
